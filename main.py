fc22c26b25598c7edcacd658db5e2180e49d1bf7
deepCode <38378077+dipkoddd@users.noreply.github.com>
2023-11-19 08:38:48 +0400

    biggest upd

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 import uvicorn
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", include_in_schema=False)
+def index():
+    return RedirectResponse("/docs", status_code=308)
 
-if __name__ == '__main__':
-    uvicorn.run(app=app)
+# if __name__ == '__main__':
+#     uvicorn.run(app=app)
